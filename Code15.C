659f2d302a000a697fb5f177059243c876538809
sorwershamim <sorwerhossain8@gmail.com>
2025-03-21 23:20:26 +0600

    Create Code15.C

---
+++ Code15.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Assignment 1: Plus, Minus, Multiplication, divide, Modules\n1(a) sum = a + b\n1(b) sum = a - b\n1(c) sum = a * b\n1(d) sum = a / b\n1(e) x percent y");
+    printf("\n\nAssignment 2:Swapping two numbers\nc = a\na = b\nb = c");
+    printf("\n\nAssignment 3: leap year\nif ((year percent 4 == 0 && year percent 100 !=0) || (year percent 400 == 0))");
+    printf("\n\nAssignment 4: Celsius to Fahrenheit and Fahrenheit to Celsius\n4(a) F = (C + 32) * (9 / 5);\n4(b) ((F - 32) * 5) / 9;");
+    printf("\n\nAssignment 5: Triangle calculation\nhypotenuse = sqrt(b * b + h * h)\narea = 0.5 * b * h\nperimeter = b + h + hypotenuse");
+    printf("\n\nAssignment 6: Calculate redius and area of a circle\n6(a) r = d / 2\n6(b) sum = 2 * 3.1416 * r");
+    printf("\n\nAssignment 7: inch to cm and cm to inch\n7(a) cm = inch * 2.54\n7(b) inch = cm / 2.54");
+    printf("\n\nAssignment 8: Find greatest number among 3 numbers\nif (a > b && a > c)\nif (b > a && b > c)\nif (c > a && c > b)");
+    printf("\n\nAssignment 9: Find smallest number among 3 numbers\nif (a < b && a < c)\nif (b < a && b < c)\nif (c < a && c < b)");
+    printf("\n\nAssignment 10: Find the prime number\nfor (i = 2; i < n; i++){if (n percent i == 0)}");
+    printf("\n\nAssignment 11: Check even or odd number\nif ( n percent 2 == 0)");
+    printf("\n\nAssignment 12: Square and Square root\n12(a) sum = n * n;\n12(b) result = sqrt(n);");
+    printf("\n\nAssignment 13: Find the area of rectangle\narea = l * w;");
+    printf("\n\nAssignment 14: Grade result(giving a example)\nif (score >= 80 && score <= 100)");
+
+    return 0;
+}
